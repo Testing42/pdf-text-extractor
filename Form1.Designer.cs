9ab452eeda218406a6d3f2e84fcfd863b3119c18
Andres Morales <43222504+Testing42@users.noreply.github.com>
2021-01-25 20:52:51 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Safeauto.txt_organizer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.TxtButton = new System.Windows.Forms.Button();
+			this.fileName = new System.Windows.Forms.TextBox();
+			this.fileLocation = new System.Windows.Forms.TextBox();
+			this.reset = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// TxtButton
+			// 
+			this.TxtButton.Location = new System.Drawing.Point(53, 21);
+			this.TxtButton.Name = "TxtButton";
+			this.TxtButton.Size = new System.Drawing.Size(85, 38);
+			this.TxtButton.TabIndex = 0;
+			this.TxtButton.Text = "Add .PDF";
+			this.TxtButton.UseVisualStyleBackColor = true;
+			this.TxtButton.Click += new System.EventHandler(this.TxtButton_Click);
+			// 
+			// fileName
+			// 
+			this.fileName.Location = new System.Drawing.Point(269, 21);
+			this.fileName.Name = "fileName";
+			this.fileName.ReadOnly = true;
+			this.fileName.Size = new System.Drawing.Size(195, 20);
+			this.fileName.TabIndex = 1;
+			// 
+			// fileLocation
+			// 
+			this.fileLocation.Location = new System.Drawing.Point(240, 82);
+			this.fileLocation.Name = "fileLocation";
+			this.fileLocation.ReadOnly = true;
+			this.fileLocation.Size = new System.Drawing.Size(322, 20);
+			this.fileLocation.TabIndex = 2;
+			// 
+			// reset
+			// 
+			this.reset.Location = new System.Drawing.Point(673, 383);
+			this.reset.Name = "reset";
+			this.reset.Size = new System.Drawing.Size(75, 23);
+			this.reset.TabIndex = 3;
+			this.reset.Text = "reset";
+			this.reset.UseVisualStyleBackColor = true;
+			this.reset.Click += new System.EventHandler(this.Reset_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(172, 21);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(54, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "File Name";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(142, 82);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(67, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "File Location";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(53, 127);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(597, 279);
+			this.richTextBox1.TabIndex = 7;
+			this.richTextBox1.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.reset);
+			this.Controls.Add(this.fileLocation);
+			this.Controls.Add(this.fileName);
+			this.Controls.Add(this.TxtButton);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button TxtButton;
+		private System.Windows.Forms.TextBox fileName;
+		private System.Windows.Forms.TextBox fileLocation;
+		private System.Windows.Forms.Button reset;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Safeauto.txt_organizer
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		
+
+		private void TxtButton_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog dlg = new OpenFileDialog();
+			string filePath;
+			dlg.Filter = "PDF Files(*.PDF)|*.PDF|All Files(*.*)|*.*";
+
+			if (dlg.ShowDialog() == DialogResult.OK)
+			{
+				filePath = dlg.FileName.ToString();
+
+
+				string strText = "";
+				try
+				{
+					PdfReader reader = new PdfReader(filePath);
+
+					for (int page = 1; page <= reader.NumberOfPages; page++)
+					{
+						ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+						String s = PdfTextExtractor.GetTextFromPage(reader, page, its);
+
+						s = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(s)));
+						strText = strText + s;
+
+						richTextBox1.Text = strText;
+						fileName.Text = dlg.SafeFileName;
+						fileLocation.Text = dlg.FileName;
+					}
+					reader.Close();
+
+
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+				}
+
+			}
+		}
+
+
+
+		private void Reset_Click(object sender, EventArgs e)
+		{
+			fileName.Text = String.Empty;
+			fileLocation.Text = String.Empty;
+			richTextBox1.Text = String.Empty;
+		}
+
+
+	}
+}
